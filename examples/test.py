d825c71e80df343eefded4cd7211c104a848d467
YannickZutter <yannick.zutter@stud.unibas.ch>
2020-09-11 15:57:56 +0200

    test file to check error

--- examples/test.py
+++ examples/test.py
@@ -18,13 +18,22 @@ ATTRIBUTES = ["coverage", "error", "expansions", "total_time", "expansions_until
 NODE = platform.node()
 if NODE.endswith(".scicore.unibas.ch") or NODE.endswith(".cluster.bc2.ch"):
     # Create bigger suites with suites.py from the downward-benchmarks repo.
-    SUITE = ["zenotravel"]
+    SUITE = [
+         'agricola-opt18-strips', 'airport', 'assembly', 'barman-opt11-strips', 'barman-opt14-strips', 'blocks',
+         'caldera-opt18-adl', 'caldera-split-opt18-adl', 'cavediving-14-adl', 'childsnack-opt14-strips',
+         'citycar-opt14-adl', 'data-network-opt18-strips', 'depot', 'driverlog', 'elevators-opt08-strips',
+         'elevators-opt11-strips', 'floortile-opt11-strips', 'floortile-opt14-strips', 'freecell',
+         #'ged-opt14-strips', 'grid', 'gripper', 'hiking-opt14-strips', 'logistics00', 'logistics98',
+         #'maintenance-opt14-adl', 'miconic', 'miconic-fulladl', 'miconic-simpleadl', 'movie', 'mprime', 'mystery',
+         #'nomystery-opt11-strips', 'nurikabe-opt18-adl', 'openstacks', 'openstacks-opt08-adl',
+         #'openstacks-opt08-strips', 'openstacks-opt11-strips', 'openstacks-opt14-strips', 'optical-telegraphs',
+         ]
 
     ENV = BaselSlurmEnvironment(email="yannick.zutter@stud.unibas.ch")
     REPO = os.path.expanduser("~/fast-downward")
 else:
     #SUITE = ["depot:p01.pddl", "gripper:prob01.pddl", "depot:p02.pddl", "gripper:prob02.pddl", "gripper:prob02.pddl",
-     #        "zenotravel:p01.pddl", "zenotravel:p02.pddl", "freecell:p01.pddl"]
+    #         "zenotravel:p01.pddl", "zenotravel:p02.pddl", "freecell:p01.pddl"]
     SUITE = ["gripper", "zenotravel"]
     ENV = LocalEnvironment(processes=2)
     REPO = os.path.expanduser("~/CLionProjects/fast-downward")
@@ -33,7 +42,7 @@ BENCHMARKS_DIR = os.path.expanduser("~/benchmarks")
 # If REVISION_CACHE is None, the default ./data/revision-cache is used.
 REVISION_CACHE = os.environ.get("DOWNWARD_REVISION_CACHE")
 VCS = cached_revision.get_version_control_system(REPO)
-REV = "1af27ae08b13298e1048e72f0e135bda67c9b579"
+REV = "7f961b6400a36f7750e6113578dabd94df50f6ef"
 
 exp = FastDownwardExperiment(environment=ENV, revision_cache=REVISION_CACHE)
 
@@ -45,10 +54,11 @@ exp.add_parser(exp.PLANNER_PARSER)
 exp.add_parser("sg-parser.py")
 
 exp.add_suite(BENCHMARKS_DIR, SUITE)
-exp.add_algorithm("default", REPO, REV, ["--search", "astar(blind(), sg = default, iteration_limit=100000)"])
-exp.add_algorithm("naive", REPO, REV, ["--search", "astar(blind(), sg = naive, iteration_limit=100000)"])
-exp.add_algorithm("marked", REPO, REV, ["--search", "astar(blind(), sg = marked, iteration_limit=100000)"])
-exp.add_algorithm("timestamps", REPO, REV, ["--search", "astar(blind(), sg = timestamps, iteration_limit=100000)"])
+exp.add_algorithm("default", REPO, REV, ["--search", "astar(blind(), sg = default)"])
+exp.add_algorithm("naive", REPO, REV, ["--search", "astar(blind(), sg = naive)"])
+exp.add_algorithm("marked", REPO, REV, ["--search", "astar(blind(), sg = marked)"])
+exp.add_algorithm("timestamps", REPO, REV, ["--search", "astar(blind(), sg = timestamps)"])
+exp.add_algorithm("psvn", REPO, REV, ["--search", "astar(blind(), sg = psvn)"])
 
 # Add step that writes experiment files to disk.
 exp.add_step("build", exp.build)
@@ -62,12 +72,6 @@ exp.add_fetcher(name="fetch")
 
 # Add report step (AbsoluteReport is the standard report).
 exp.add_report(AbsoluteReport(attributes=ATTRIBUTES), outfile="report.html")
-
-# Add scatter plot report step.
-exp.add_report(
-    ScatterPlotReport(attributes=["expansions"], filter_algorithm=["default", "naive", "marked", "timestamps", ]),
-    outfile="scatterplot.png",
-)
 
 # Parse the commandline and show or run experiment steps.
 exp.run_steps()
